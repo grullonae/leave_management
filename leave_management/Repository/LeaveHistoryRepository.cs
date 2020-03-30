2c93f244a229fd27b4d1872194c6e9e3116dbd22
Adriano Grullon <grullonae@gmail.com>
2020-03-30 10:25:59 -0700

    Added LeaveType Repositories and Added Databases and Font Awesome Icons

---
+++ leave_management/Repository/LeaveHistoryRepository.cs
@@ -0,0 +1,59 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveHistoryRepository : ILeaveHistoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveHistoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool Create(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Add(entity);
+            //Save
+            return Save();
+        }
+
+        public bool Delete(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Remove(entity);
+            //Save
+            return Save();
+        }
+
+        public ICollection<LeaveHistory> FindAll()
+        {
+            var LeaveHistory = _db.LeaveHistories.ToList();
+            return LeaveHistory;
+        }
+
+        public LeaveHistory FindById(int id)
+        {
+            var LeaveHistory = _db.LeaveHistories.Find(id);
+            return LeaveHistory;
+        }
+
+        public bool Save()
+        {
+            //return _db.SaveChanges() > 0;
+            var changes = _db.SaveChanges();
+            return changes > 0;
+        }
+
+        public bool Update(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Update(entity);
+            //Save
+            return Save();
+        }
+    }
+}
